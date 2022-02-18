7ec5476beceb65c43fa995fca6a5f06ce8f46a0e
Vikkelas <bass89@mail.ru>
2022-02-19 01:06:20 +0300

    only one req: allTikets

--- server.js
+++ server.js
@@ -19,12 +19,47 @@ app.use(koaBody({
    urlencoded: true,
 }));
 
+app.use(async (ctx, next) => {
+   const origin = ctx.request.get('http://localhost:8080');
+   if (!origin) {
+      return await next();
+   }
+   const headers = {
+      'Access-Control-Allow-Origin': '*',
+   };
+
+   if (ctx.request.method !== 'OPTIONS') {
+      ctx.response.set({
+         ...headers
+      });
+      try {
+         return await next();
+      } catch (e) {
+         e.headers = {
+            ...e.headers,
+            ...headers
+         };
+         throw e;
+      }
+   }
+
+   if (ctx.request.get('Access-Control-Request-Method')) {
+      ctx.response.set({
+         ...headers,
+         'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
+      });
+      if (ctx.request.get('Access-Control-Request-Headers')) {
+         ctx.response.set('Access-Control-Request-Headers', ctx.request.get('Access-Control-Allow-Request-Headers'));
+      }
+      ctx.response.status = 204;
+   }
+});
+
 app.use(async (ctx) => {
    const {
       method
    } = ctx.request.query;
    if (ctx.request.method === 'GET' && method === 'allTikets') {
-      console.log(ctx.request.headers);
       ctx.response.body = tikets;
    }
 });
