bc77c75ea129bafcef4196addae4ae1c2e932ebd
vikkelas <bass89@mail.ru>
2022-02-28 14:12:01 +0300

    add cors

--- server.js
+++ server.js
@@ -24,42 +24,6 @@ app.use(koaBody({
    json: true,
 }));
 
-// app.use(async (ctx, next) => {
-//    const origin = ctx.request.get('Origin');
-//    if (!origin) {
-//       return await next();
-//    }
-//    const headers = {
-//       'Access-Control-Allow-Origin': '*',
-//    };
-
-//    if (ctx.request.method !== 'OPTIONS') {
-//       ctx.response.set({
-//          ...headers
-//       });
-//       try {
-//          return await next();
-//       } catch (e) {
-//          e.headers = {
-//             ...e.headers,
-//             ...headers
-//          };
-//          throw e;
-//       }
-//    }
-
-//    if (ctx.request.get('Access-Control-Request-Method')) {
-//       ctx.response.set({
-//          ...headers,
-//          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH',
-//       });
-//       if (ctx.request.get('Access-Control-Request-Headers')) {
-//          ctx.response.set('Access-Control-Request-Headers', ctx.request.get('Access-Control-Allow-Request-Headers'));
-//       }
-//       ctx.response.status = 204;
-//    }
-// });
-
 app.use(async (ctx) => {
    const {
       method,
